6c0baa1b6a00cf96daca6aa0013ed45d1916512b
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-16 22:32:34 +0500

    Max2

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -10,12 +10,6 @@ public class MaxTest {
         Max max = new Max();
         int result = max.max(1, 2);
         assertThat(result, is(2));
-
-        result = max.max(2,1);
-        assertThat(result, is(2));
-
-        result = max.max(2,2);
-        assertThat(result, is(2));
     }
     @Test
     public void whenMax2To1Then2(){
