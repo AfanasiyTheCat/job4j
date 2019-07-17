a51907e6d243469b9b3ddd765193998477b22d77
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-17 17:18:03 +0500

    Counter2

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,6 +1,21 @@
 package ru.job4j.loop;
 
+
+/**
+ * Counter.
+ *
+ * @author Nikita Shvetsov (mrdevislife2@mail.ru)
+ * @version $1.0v$
+ * @since 17.07.2019
+ */
+
 public class Counter {
+    /**
+     * Method add
+     * @param start - first number
+     * @param finish - last number
+     * @return count of чётные numbers
+     */
     public int add(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++){
