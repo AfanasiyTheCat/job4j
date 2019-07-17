d69942e9d24fff6a33a92d94e58f52776179cef2
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-17 11:58:08 +0500

    Counter

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter check = new Counter();
+        int expected = 30;
+        assertThat(check.add(1,10), is(expected));
+    }
+}
