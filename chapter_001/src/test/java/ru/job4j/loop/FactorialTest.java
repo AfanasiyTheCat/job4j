5d18f7774288b1361e859dd81bcfec5ff6334f35
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-17 12:02:27 +0500

    Factorial

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        //напишите здесь тест, проверяющий, что факториал для числа 5 равен 120.
+        Factorial check = new Factorial();
+        assertThat(check.calc(5),is(120));
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        //напишите здесь тест, проверяющий, что факториал для числа 0 равен 1.
+        Factorial check = new Factorial();
+        assertThat(check.calc(0),is(1));
+    }
+}
