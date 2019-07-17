df2ab4b1c5c2110a46340ede56401e973bf7c6ec
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-17 17:52:41 +0500

    Fitness2

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -1,6 +1,19 @@
 package ru.job4j.loop;
 
+/**
+ * BoardTest.
+ *
+ * @author Nikita Shvetsov (mrdevislife2@mail.ru)
+ * @version $1.0v$
+ * @since 17.07.2019
+ */
 public class Fitness {
+    /**
+     * Method calc
+     * @param ivan - ivan's mass
+     * @param nik - nik's mass
+     * @return moths, after which
+     */
     public int calc(int ivan, int nik) {
         int month = 0;
         for (; ivan <= nik; month++){
