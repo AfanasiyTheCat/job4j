3521355474194926750a053b7150870bc2b2ea57
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-17 17:22:14 +0500

    Update Factorial.java

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,6 +1,19 @@
 package ru.job4j.loop;
 
+/**
+ * Factorial.
+ *
+ * @author Nikita Shvetsov (mrdevislife2@mail.ru)
+ * @version $1.0v$
+ * @since 17.07.2019
+ */
+
 public class Factorial {
+    /**
+     *
+     * @param n - number for factorial
+     * @return factorial of n number
+     */
     public int calc(int n) {
         int result = 1;
         for (int i = n; i >= 1; i--){
