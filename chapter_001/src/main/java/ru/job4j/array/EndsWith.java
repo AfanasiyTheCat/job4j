f27416d2b5c85d2b5075cc4a5a9db371bbd92319
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-18 15:51:49 +0500

    EndsWith

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * @author Nikita Shvetsov
+ * @version 1.0v
+ * @since 18.07.2019
+ */
+public class EndsWith {
+
+    /**
+     * Проверяет. что слово заканчивается на префик.
+     * @param post префикс.
+     * @return если слово заканчивается с префикс
+     */
+    public boolean endsWith(String word, String post) {
+        boolean result = true;
+        char[] pst = post.toCharArray();
+        char[] wrd = word.toCharArray();
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        for (int i = 0; i < pst.length; i++){
+            if (wrd[wrd.length - i - 1] != pst[pst.length - i - 1]){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
