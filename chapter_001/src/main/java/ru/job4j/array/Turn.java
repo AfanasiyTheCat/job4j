adfa8d9676193c3bd0879d05940ecb3d0662147d
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-18 15:28:41 +0500

    Turn

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ * @author Nikita Shvetsov
+ * @version 1.0v
+ * @since 18.07.2019
+ */
+public class Turn {
+    /**
+     * Method back
+     * @param array - массив
+     * @return - перевёрнутый массив
+     */
+    public int[] back(int[] array) {
+        int center = array.length % 2 == 0 ? array.length / 2 : (array.length - 1) / 2;
+        for (int i = 0; i < center; i++) {
+            int x = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = x;
+        }
+        return array;
+    }
+}
