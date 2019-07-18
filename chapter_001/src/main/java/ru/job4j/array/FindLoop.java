7ca64aff2f0c8fe1d22357b038b8be371dd88c4f
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-18 16:11:44 +0500

    FindLoop - sort

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -22,6 +22,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    /**
+     *
+     * @param data - массив для поиска
+     * @param el - элемент,который нужно найти
+     * @param start - индекс, с которого начинать поиск
+     * @param finish - индекс, которым заканчивать поиск
+     * @return - индекс нужного элемента
+     */
     public int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int i = start; i <= finish; i++){
@@ -31,5 +40,23 @@ public class FindLoop {
             }
         }
         return rst;
+    }
+
+    /**
+     *
+     * @param data - массив для сортировка
+     * @return - отсортированный массив
+     */
+    public int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++){
+            int min = data[i];
+            for (int j = i + 1; j < data.length; j++){
+                min = data[j] < min ? data[j] : min;
+            }
+            int x = data[this.indexOf(data,min,i,data.length- 1)];
+            data[this.indexOf(data,min,i,data.length- 1)] = data[i];
+            data[i] = x;
+        }
+        return data;
     }
 }
