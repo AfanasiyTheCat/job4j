1fb325a4aca1ec83ce036a9d190d4ccb4d655dbf
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-07-29 13:56:56 +0500

    Final edit

--- chapter_001/src/test/java/ru/job4j/array/FinalTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FinalTest.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FinalTest {
     @Test
@@ -19,6 +19,38 @@ public class FinalTest {
         int[] input1 = new int[]{1, 5, 8, 11};
         int[] input2 = new int[]{2, 4, 10, 18};
         int[] expected = new int[]{1, 2, 4, 5, 8, 10, 11, 18};
+        assertThat(test.sort(input1, input2), is(expected));
+    }
+    @Test
+    public void when4and1isless() {
+        Final test = new Final();
+        int[] input1 = new int[]{1, 2, 3, 4};
+        int[] input2 = new int[]{5, 6, 7, 8};
+        int[] expected = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        assertThat(test.sort(input1, input2), is(expected));
+    }
+    @Test
+    public void when4and21isless() {
+        Final test = new Final();
+        int[] input1 = new int[]{5, 6, 7, 8};
+        int[] input2 = new int[]{1, 2, 3, 4};
+        int[] expected = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        assertThat(test.sort(input1, input2), is(expected));
+    }
+    @Test
+    public void when4and3() {
+        Final test = new Final();
+        int[] input1 = new int[]{5, 6, 7, 8};
+        int[] input2 = new int[]{1, 2, 3};
+        int[] expected = new int[]{1, 2, 3, 5, 6, 7, 8};
+        assertThat(test.sort(input1, input2), is(expected));
+    }
+    @Test
+    public void when6and2() {
+        Final test = new Final();
+        int[] input1 = new int[]{2, 5, 6, 7, 8, 10};
+        int[] input2 = new int[]{1, 3};
+        int[] expected = new int[]{1, 2, 3, 5, 6, 7, 8, 10};
         assertThat(test.sort(input1, input2), is(expected));
     }
 }
