c9fece2894c3103ce2c3944f612a006b18822b89
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-08-05 11:07:47 +0500

    ConvertMatrix2List

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -5,17 +5,22 @@ import java.util.List;
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = (list.size() + (rows - list.size() % rows)) / rows ;
-        int cell = 0;
         int[][] array = new int[rows][cells];
-        for (int[] i : array) {
-            for (int j = 0; j < i.length; j++) {
-                if (cell < list.size()) {
-                    i[j] = list.get(cell);
-                } else {
-                    i[j] = 0;
-                }
-                cell++;
+        int x = -1, y = 0;
+        for (int i = 0; i <= list.size(); i++) {
+            if (x < cells - 1) {
+                x++;
+            } else {
+                y++;
+                x = 0;
             }
+            if (y == rows - 1 && i == list.size()) {
+                for (; x < cells - 1; x++) {
+                    array[y][x] = 0;
+                }
+                break;
+            }
+            array[y][x] = list.get(i);
         }
         return array;
     }
