f1d19ff47e2a2eb9123d64893158da26efae8cef
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-08-13 10:56:57 +0500

    Рефакторинг

--- chapter_003/src/test/java/ru/job4j/usermap/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/usermap/SortUserTest.java
@@ -10,31 +10,20 @@ import static org.hamcrest.Matchers.is;
 public class SortUserTest {
     @Test
     public void ThreeUsers() {
-        List<User> list = new ArrayList<>();
         User o1 = new User("nikita", 18);
         User o2 = new User("Alex", 23);
         User o3 = new User("Maria", 17);
-        list.add(o1);
-        list.add(o2);
-        list.add(o3);
+        List<User> list = List.of(o1, o2, o3);
         SortUser sort = new SortUser();
-        Set<User> expected = new TreeSet<>();
-        expected.addAll(
-                Arrays.asList(
-                        o3, o1 , o2
-                )
-        );
+        Set<User> expected = Set.of(o3, o1 , o2);
         assertThat(sort.sort(list), is(expected));
     }
     @Test
     public void ThreeUsersTwoOfThenEqual() {
-        List<User> list = new ArrayList<>();
         User o1 = new User("nikita", 18);
         User o2 = new User("Alex", 23);
         User o3 = new User("nikita", 18);
-        list.add(o1);
-        list.add(o2);
-        list.add(o3);
+        List<User> list = List.of(o1, o2, o3);
         SortUser sort = new SortUser();
         Set<User> expected = new TreeSet<>();
         expected.addAll(
