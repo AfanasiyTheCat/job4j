17333f64da96cf9e410012b9dadaf3cbefce4508
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-08-13 10:50:42 +0500

    Student с улучшениями Stream API

--- chapter_004/src/main/java/ru/job4j/filter/Student.java
+++ chapter_004/src/main/java/ru/job4j/filter/Student.java
@@ -1,13 +1,38 @@
 package ru.job4j.filter;
 
-public class Student {
-    private int score;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    public Student(int score) {
-        this.score = score;
+public class Student {
+    private int scope;
+    private String name;
+
+    public Student(int score, String name) {
+        this.scope = score;
+        this.name = name;
     }
 
-    public int getScore() {
-        return score;
+    public int getScope() {
+        return scope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .sorted(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }).flatMap(Stream::ofNullable)
+                .takeWhile(student -> student.getScope() > bound)
+                .map(student -> student)
+                .collect(Collectors.toList());
+
     }
 }
