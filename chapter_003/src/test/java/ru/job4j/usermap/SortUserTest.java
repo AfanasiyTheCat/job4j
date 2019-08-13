ce91a7189375114e583739f59ee8502f5d2a5e05
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-08-13 11:00:44 +0500

    Update SortUserTest.java

--- chapter_003/src/test/java/ru/job4j/usermap/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/usermap/SortUserTest.java
@@ -10,73 +10,43 @@ import static org.hamcrest.Matchers.is;
 public class SortUserTest {
     @Test
     public void ThreeUsersByLength() {
-        List<User> list = new ArrayList<>();
         User o1 = new User("nikita", 18);
         User o2 = new User("Alex", 23);
         User o3 = new User("Maria", 17);
-        list.add(o1);
-        list.add(o2);
-        list.add(o3);
+        List<User> list = List.of(o1, o2, o3);
         SortUser sort = new SortUser();
-        Set<User> expected = new TreeSet<>();
-        expected.addAll(
-                Arrays.asList(
-                        o3, o1 , o2
-                )
-        );
+        Set<User> expected = Set.of(o3, o1, o2);
         assertThat(sort.sort(list), is(expected));
     }
     @Test
     public void ThreeUsersTwoOfThenEqual() {
-        List<User> list = new ArrayList<>();
         User o1 = new User("nikita", 18);
         User o2 = new User("Alex", 23);
         User o3 = new User("nikita", 18);
-        list.add(o1);
-        list.add(o2);
-        list.add(o3);
+        List<User> list = List.of(o1, o2, o3);
         SortUser sort = new SortUser();
-        Set<User> expected = new TreeSet<>();
-        expected.addAll(
-                Arrays.asList(
-                        o1, o3 , o2
-                )
-        );
+        Set<User> expected = Set.of(o1, o3, o2);
         assertThat(sort.sort(list), is(expected));
     }
     @Test
     public void ThreeUsersByName() {
-        List<User> list = new ArrayList<>();
         User o1 = new User("Alex", 18);
         User o2 = new User("Tom", 23);
         User o3 = new User("Maria", 17);
-        list.add(o1);
-        list.add(o2);
-        list.add(o3);
+        List<User> list = List.of(o1, o2, o3);
         SortUser sort = new SortUser();
-        List<User> expected = new ArrayList<>();
-        expected.add(o2);
-        expected.add(o1);
-        expected.add(o3);
+        Set<User> expected = Set.of(o2, o1, o3);
         assertThat(sort.sortNameLength(list), is(expected));
     }
     @Test
     public void FourUsersByAll() {
-        List<User> list = new ArrayList<>();
         User o1 = new User("Сергей", 25);
         User o2 = new User("Иван", 30);
         User o3 = new User("Сергей", 20);
         User o4 = new User("Иван", 25);
-        list.add(o1);
-        list.add(o2);
-        list.add(o3);
-        list.add(o4);
+        List<User> list = List.of(o1, o2, o3, o4);
         SortUser sort = new SortUser();
-        List<User> expected = new ArrayList<>();
-        expected.add(o4);
-        expected.add(o2);
-        expected.add(o3);
-        expected.add(o1);
+        Set<User> expected = Set.of(o4, o2, o3, o1);
         assertThat(sort.sortByAllFields(list), is(expected));
     }
 }
