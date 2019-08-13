ef89a208868c9baca04bad7b0a31be8f97de48d5
AfanasiyTheCat <mrdevislife2@mail.ru>
2019-08-13 11:02:53 +0500

    Рефакторинг List.of

--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -14,7 +14,7 @@ public class ConvertList2ArrayTest {
     public void when7ElementsThen9() {
         ConvertList2Array list = new ConvertList2Array();
         int[][] result = list.toArray(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                List.of(1, 2, 3, 4, 5, 6, 7),
                 3
         );
         int[][] expect = {
@@ -30,13 +30,7 @@ public class ConvertList2ArrayTest {
         List<int[]> list = new ArrayList<>();
         list.add(new int[]{1, 2});
         list.add(new int[]{3, 4, 5, 6});
-        List<Integer> expected = new ArrayList<>();
-        expected.add(1);
-        expected.add(2);
-        expected.add(3);
-        expected.add(4);
-        expected.add(5);
-        expected.add(6);
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
         assertThat(convert.convert(list).get(4), is(expected.get(4)));
     }
 }
